ed12a1a0bfab0f5b7b3f4dad53936430b9682ea3
QA <aishams83@mail.ru>
2025-05-29 14:25:18 +0300

    homework2

--- test2/test_hh.py
+++ test2/test_hh.py
@@ -1,17 +1,6 @@
 import pytest
 from selene import browser, have, by
 
-
-
-def test_2():
-    browser.open('https://school.qa.guru')
-    browser.element('[name="email"]').type('qagurubot@gmail.com')
-    browser.element('[name="password"]').type('somepasshere').press_enter()
-    browser.element('[class="page-header"]').should(have.text('Список тренингов'))
-    browser.open('https://school.qa.guru/cms/system/login')
-    browser.element('.logined-form').should(have.text('QA_GURU_BOT'))
-    browser.close()
-
 def test_2_wrong_login_password():
     browser.open('https://school.qa.guru')
     browser.element('[name="email"]').type('qagurubot@gmail.com')
